520262b4a9fc9fbcf2b1db5af53a131187539bce
Markevo5150 <marcoAlopez2@outlook.com>
2023-11-01 14:01:18 -0600

    Project start

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="header">
+          <h1 className="header__title">
+            10,000+ of our users love our products.
+          </h1>
+          <p className="header__info">
+            We only provide great products combined with excellent customer
+            service. See what our satisfied customers are saying about our
+            services.
+          </p>
+        </div>
+        <div className="rating-section"></div>
+      </div>
+    </>
+  );
+}
