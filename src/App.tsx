76cb8088a3c67c64e92cc4bea130e5e42b173883
Markevo5150 <marcoAlopez2@outlook.com>
2023-11-08 10:42:42 -0600

    FIles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Rate from "./components/rate/Rate";
+import Testimony from "./components/testimony/Testimony";
 export default function App() {
   return (
     <>
@@ -17,6 +18,9 @@ export default function App() {
           <Rate stars={5}>Rated 5 stars in Reviews</Rate>
           <Rate stars={5}>Rated 5 stars in Report Guru</Rate>
           <Rate stars={5}>Rated 5 stars in BestTech</Rate>
+        </div>
+        <div className="comment-section">
+          <Testimony name="Colton Smith" comment="Put an order"></Testimony>
         </div>
       </div>
     </>
---
+++ src/components/testimony/Testimony.tsx
@@ -0,0 +1,15 @@
+import classNames from "classnames";
+import { Props } from "./types";
+import "./Testimony.scss";
+
+export default function Testimony({ image, name, userTitle, comment }: Props) {
+  return (
+    <div className={classNames("testimony")}>
+      <div className="img"></div>
+      <div className="title">
+        <h2>{name}</h2>
+        <h3>{userTitle}</h3>
+      </div>
+    </div>
+  );
+}
